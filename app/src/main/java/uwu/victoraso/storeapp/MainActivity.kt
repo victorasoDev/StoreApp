52080b6de2bd32070531e88393dc97df0f30df08
victorasoDev <victoraso.developer@gmail.com>
2022-07-11 12:01:06 +0200

    first commit

---
+++ app/src/main/java/uwu/victoraso/storeapp/MainActivity.kt
@@ -0,0 +1,38 @@
+package uwu.victoraso.storeapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import uwu.victoraso.storeapp.ui.theme.StoreAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            StoreAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    StoreAppTheme {
+        Greeting("Android")
+    }
+}
