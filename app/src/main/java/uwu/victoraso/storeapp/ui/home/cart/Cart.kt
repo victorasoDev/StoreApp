4c4e59c492f0819b6188adce630607abb83c5900
victorasoDev <victoraso.developer@gmail.com>
2022-10-08 16:34:30 +0200

    cambio en el layout del cart item, falta controlar que no se pueda añadir el mismo producto

--- app/src/main/java/uwu/victoraso/storeapp/ui/home/cart/Cart.kt
+++ app/src/main/java/uwu/victoraso/storeapp/ui/home/cart/Cart.kt
@@ -1,47 +1,42 @@
 package uwu.victoraso.storeapp.ui.home.cart
 
 import android.content.res.Configuration
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.*
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.ShoppingCartCheckout
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.BottomCenter
+import androidx.compose.ui.Alignment.Companion.Center
+import androidx.compose.ui.Alignment.Companion.CenterEnd
+import androidx.compose.ui.Alignment.Companion.CenterStart
+import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.ui.Alignment.Companion.Start
+import androidx.compose.ui.Alignment.Companion.Top
+import androidx.compose.ui.Alignment.Companion.TopEnd
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.LastBaseline
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.compose.ChainStyle
-import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.google.android.material.behavior.SwipeDismissBehavior
 import uwu.victoraso.storeapp.R
 import uwu.victoraso.storeapp.model.CartProduct
-import uwu.victoraso.storeapp.model.OrderLine
-import uwu.victoraso.storeapp.model.ProductCollection
-import uwu.victoraso.storeapp.model.ProductRepo
 import uwu.victoraso.storeapp.ui.components.*
 import uwu.victoraso.storeapp.ui.home.DestinationBar
-import uwu.victoraso.storeapp.ui.home.HomeSections
-import uwu.victoraso.storeapp.ui.theme.AlphaNearOpaque
 import uwu.victoraso.storeapp.ui.theme.StoreAppTheme
 import uwu.victoraso.storeapp.ui.utils.formatPrice
 
@@ -187,48 +182,51 @@ fun CartItem(
     onProductClick: (Long, String) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    ConstraintLayout(
+    Box(
         modifier = modifier
-            .fillMaxWidth()
             .clickable { onProductClick(cartProduct.productId, cartProduct.category) }
+            .fillMaxWidth()
             .background(StoreAppTheme.colors.uiBackground)
-            .padding(horizontal = 24.dp)
+            .fillMaxWidth()
     ) {
-        val (divider, image, name, tag, priceSpacer, price, remove, quantity) = createRefs()
-        createVerticalChain(name, tag, priceSpacer, price, chainStyle = ChainStyle.Packed)
-        ProductImage(
-            imageUrl = cartProduct.imageUrl,
-            contentDescription = null,
-            modifier = Modifier
-                .size(100.dp)
-                .constrainAs(image) {
-                    top.linkTo(parent.top, margin = 16.dp)
-                    bottom.linkTo(parent.bottom, margin = 16.dp)
-                    start.linkTo(parent.start)
-                }
-        )
-        Text(
-            text = cartProduct.name,
-            style = MaterialTheme.typography.subtitle1,
-            color = StoreAppTheme.colors.textSecondary,
-            modifier = Modifier.constrainAs(name) {
-                linkTo(
-                    start = image.end,
-                    startMargin = 16.dp,
-                    end = remove.start,
-                    endMargin = 16.dp,
-                    bias = 0f
+        Row(
+            modifier = Modifier.padding(all = 16.dp),
+            verticalAlignment = CenterVertically
+        ) {
+            ProductImage(
+                imageUrl = cartProduct.imageUrl,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(100.dp)
+            )
+            Column(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(end = 24.dp),
+                verticalArrangement = Arrangement.spacedBy(14.dp)
+            ) {
+                Text(
+                    text = cartProduct.name,
+                    style = MaterialTheme.typography.subtitle1,
+                    color = StoreAppTheme.colors.textSecondary,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 2,
+                    modifier = Modifier
+                        .padding(start = 8.dp, end = 16.dp)
+
+                )
+                Text(
+                    text = formatPrice(cartProduct.price),
+                    style = MaterialTheme.typography.subtitle1,
+                    color = StoreAppTheme.colors.textPrimary,
+                    modifier = Modifier.padding(start = 8.dp, end = 16.dp)
                 )
             }
-        )
+        }
         IconButton(
             onClick = { removeProduct(cartProduct.productId, cartProduct.cartId) },
-            modifier = Modifier
-                .constrainAs(remove) {
-                    top.linkTo(parent.top)
-                    end.linkTo(parent.end)
-                }
-                .padding(top = 12.dp)
+            modifier = modifier
+                .align(TopEnd)
         ) {
             Icon(
                 imageVector = Icons.Filled.Close,
@@ -236,47 +234,7 @@ fun CartItem(
                 tint = StoreAppTheme.colors.iconSecondary
             )
         }
-        Text(
-            text = "A tagline",
-            style = MaterialTheme.typography.body1,
-            color = StoreAppTheme.colors.textHelp,
-            modifier = Modifier.constrainAs(tag) {
-                linkTo(
-                    start = image.end,
-                    startMargin = 16.dp,
-                    endMargin = 16.dp,
-                    end = parent.end,
-                    bias = 0f
-                )
-            }
-        )
-        Spacer(
-            modifier = Modifier
-                .height(8.dp)
-                .constrainAs(priceSpacer) {
-                    linkTo(top = tag.bottom, bottom = price.top)
-                }
-        )
-        Text(
-            text = formatPrice(cartProduct.price),
-            style = MaterialTheme.typography.subtitle1,
-            color = StoreAppTheme.colors.textPrimary,
-            modifier = Modifier.constrainAs(price) {
-                linkTo(
-                    start = image.end,
-                    end = quantity.start,
-                    startMargin = 16.dp,
-                    endMargin = 16.dp,
-                    bias = 0f
-                )
-            }
-        )
-        StoreAppDivider(
-            Modifier.constrainAs(divider) {
-                linkTo(start = parent.start, end = parent.end)
-                top.linkTo(parent.bottom)
-            }
-        )
+        StoreAppDivider(modifier = modifier.align(BottomCenter))
     }
 }
 
@@ -365,6 +323,9 @@ fun SummaryItem(
 @Composable
 private fun CartPreview() {
     StoreAppTheme {
-        //TODO:
+        CartItem(
+            cartProduct = CartProduct(name = "Gygabyte GeForce RTX 3080", price = 1234323),
+            removeProduct = { a, b -> },
+            onProductClick = { a, b -> })
     }
 }
